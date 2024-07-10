1ab026e7032d029cfb7e110003bbb10c1c2be7fa
Megh Deb <20169629-pheonixflaunt@users.noreply.gitlab.com>
2024-07-10 17:43:06 +0530

    LOL

---
+++ views/food.py
@@ -0,0 +1,64 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_repsonse(input, image, prompt):
+    model = genai.GenerativeModel("gemini-pro-vision")
+    response = model.generate_content([input, image[0], prompt])
+    return response.text
+
+
+def input_image_setup(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data,
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+st.header("Food Analyzer AI")
+input = st.text_input("", key="input",disabled=True)
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+image = ""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+
+submit = st.button("Analyze Food")
+
+input_prompt = """
+You are an expert in nutritionist where you need to see the food items from the image
+               and calculate the total calories, say if the food is healthy or not and also provide the details of every food items with calories intake
+               is below format
+
+               1. Item 1 - no of calories
+               2. Item 2 - no of calories
+               ----
+               ----
+            Say if the food is healthy or not at last
+
+
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response = get_gemini_repsonse(input_prompt, image_data, input)
+    st.subheader("The Response is")
+    st.write(response)
